9453647b89d1526b3a49ad9e41ef5d106440b7cb
Ankur Srivastava <ankursrivastava@Ankurs-MacBook-Air.local>
2024-10-04 16:10:03 +0530

    message

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -4,8 +4,10 @@ async function main() {
   const newUser: NewUser = {
     email: "foo@example.com",
     image: "some image url",
-    name: "foo",
+    first_name: "foo",  // Change 'name' to 'first_name'
+    last_name: "bar",   // Add 'last_name'
   };
+  
   const res = await insertUser(newUser);
   console.log("insert user success", res);
   process.exit();
