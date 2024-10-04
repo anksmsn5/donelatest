a5ea0abc16596521085fea07517aca016a14d63e
Ankur Srivastava <ankursrivastava@Ankurs-MacBook-Air.local>
2024-10-04 10:37:54 +0530

    brand Isssue Reslved 1

--- app/components/Brand.tsx
+++ app/components/Brand.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
-import '../globals.css'; 
-import Logo from '../public/images/logo.png';
-import Image from 'next/image';
+
+
 
 const Brand: React.FC = () => {
   return (
-    <>
-      <span>NA</span>
-    </>
+    
+      <div>NA</div>
+    
   );
 };
 
