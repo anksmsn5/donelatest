8be4025b7e7b9dcab1e969ff11f30187436e4938
Ankur Srivastava <ankursrivastava@Ankurs-MacBook-Air.local>
2024-10-04 16:15:14 +0530

    message

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -6,6 +6,16 @@ async function main() {
     image: "some image url",
     first_name: "foo",  // Change 'name' to 'first_name'
     last_name: "bar",   // Add 'last_name'
+    grade_level: "bar",   // Add 'last_name'
+    location: "bar",   // Add 'last_name'
+    birthday: "bar",   // Add 'last_name'
+    gender: "bar",   // Add 'last_name'
+    sport: "bar",   // Add 'last_name'
+    team: "bar",   // Add 'last_name'
+    position: "bar",   // Add 'last_name'
+    number: "bar",   // Add 'last_name'
+    password: "bar",   // Add 'last_name'
+    createdAt:Date(),   // Add 'last_name'
   };
   
   const res = await insertUser(newUser);
