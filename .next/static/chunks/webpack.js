1e5a5e782b250485020f62fc43c622bd125c3b46
Ankur Srivastava <ankursrivastava@Ankurs-MacBook-Air.local>
2024-10-11 14:18:43 +0530

    message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -199,7 +199,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7f762d58bc6b8f32"; }
+/******/ 		__webpack_require__.h = function() { return "5c8c109b6979e54d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/api/coach/evaluations/route.ts
+++ app/api/coach/evaluations/route.ts
@@ -1,20 +1,12 @@
 // app/api/register/route.ts
 
 import { NextRequest, NextResponse } from 'next/server';
-import { hash } from 'bcryptjs';
 import { db } from '../../../../lib/db';
-import { playerEvaluation, users, coaches } from '../../../../lib/schema'
-import { like } from 'drizzle-orm';
+import { playerEvaluation, users, coaches } from '../../../../lib/schema';
 import { eq } from 'drizzle-orm';
-import { getServerSession } from 'next-auth';
-import next from 'next';
-
-
-
 
 export async function POST(req: NextRequest) {
   try {
-
     const { userId, status } = await req.json();
 
     const evaluationsData = await db
@@ -25,13 +17,17 @@ export async function POST(req: NextRequest) {
         number: users.number,
         image: users.image,
         team: users.team,
-        coachPhoto:coaches.image,
-        expectedCharge:coaches.expectedCharge,
-        ...playerEvaluation,
+        coachPhoto: coaches.image,
+        expectedCharge: coaches.expectedCharge,
+        evaluationId: playerEvaluation.id, // Select specific columns from playerEvaluation
+        reviewTitle: playerEvaluation.review_title, 
+        evaluationStatus: playerEvaluation.status,
+        createdAt: playerEvaluation.created_at,
+        updatedAt: playerEvaluation.updated_at,
       })
       .from(playerEvaluation)
-      .innerJoin(users, eq(playerEvaluation.player_id, users.id)) // Assuming user_id is the foreign key in playerEvaluation
-      .innerJoin(coaches, eq(playerEvaluation.coach_id, coaches.id)) // Assuming user_id is the foreign key in playerEvaluation
+      .innerJoin(users, eq(playerEvaluation.player_id, users.id)) // Assuming player_id is the foreign key in playerEvaluation
+      .innerJoin(coaches, eq(playerEvaluation.coach_id, coaches.id)) // Assuming coach_id is the foreign key in playerEvaluation
       .where(eq(playerEvaluation.coach_id, userId))
       .where(eq(playerEvaluation.status, status))
       .limit(10) // Limit the number of results to 10
@@ -40,40 +36,31 @@ export async function POST(req: NextRequest) {
     return NextResponse.json(evaluationsData);
 
   } catch (error) {
-    return NextResponse.json(
-      { message: error },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: error }, { status: 500 });
   }
 }
 
 export async function PUT(req: NextRequest) {
   try {
-
     const { evaluationId, status } = await req.json();
     const parsedEvaluationId = parseInt(evaluationId, 10);
-    
+
     const result = await db
       .update(playerEvaluation)
-      .set({ status:status || undefined }) // Set the new status value
+      .set({ status: status || undefined }) // Set the new status value
       .where(eq(playerEvaluation.id, parsedEvaluationId)) // Condition for evaluation ID
       .returning();
 
     return NextResponse.json("Success");
 
   } catch (error) {
-    return NextResponse.json(
-      { message: error },
-      { status: 500 }
-    );
+    return NextResponse.json({ message: error }, { status: 500 });
   }
 }
 
-
-export async function GET(request: NextRequest) {
+export async function GET(req: NextRequest) {
   try {
-    const url = request.nextUrl;
-
+    const url = req.nextUrl;
     const playerId = Number(url.searchParams.get('playerId'));
     const status = url.searchParams.get('status');
     const search = url.searchParams.get('search') || '';
@@ -81,23 +68,22 @@ export async function GET(request: NextRequest) {
     const limit = Number(url.searchParams.get('limit')) || 10;
     const sort = url.searchParams.get('sort') || '';
 
-    if (isNaN(playerId)){
+    if (isNaN(playerId)) {
       return NextResponse.json({ error: 'Invalid query parameters' }, { status: 400 });
     }
 
-    console.log('Query parameters:', { playerId, status, search, page, limit, sort });
-
-    console.log('Before executing query...');
     let query = db
       .select({
-        firstName: coaches.firstName,
-        lastName: coaches.lastName,
-        ...playerEvaluation,
+        firstName: coaches.first_name,
+        lastName: coaches.last_name,
+        evaluationId: playerEvaluation.id, // Select specific columns
+        reviewTitle: playerEvaluation.review_title,
+        evaluationStatus: playerEvaluation.status,
+        createdAt: playerEvaluation.created_at,
       })
       .from(playerEvaluation)
       .innerJoin(coaches, eq(playerEvaluation.coach_id, coaches.id))
       .where(eq(playerEvaluation.player_id, playerId));
-
 
     if (status) {
       query = query.where(eq(playerEvaluation.status, status));
@@ -105,37 +91,36 @@ export async function GET(request: NextRequest) {
 
     // Execute the query with a limit
     const evaluationsData = await query.limit(limit).execute();
-    let filteredData: Evaluation[] = evaluationsData;
+    let filteredData = evaluationsData;
 
     // Filter data based on search
     if (search) {
       filteredData = filteredData.filter(item =>
-        item.firstName.toLowerCase().includes((search as string).toLowerCase()) ||
-        item.lastName.toLowerCase().includes((search as string).toLowerCase()) ||
-        item.review_title.toLowerCase().includes((search as string).toLowerCase()) ||
-        item.video_description.toLowerCase().includes((search as string).toLowerCase())
+        item.firstName.toLowerCase().includes(search.toLowerCase()) ||
+        item.lastName.toLowerCase().includes(search.toLowerCase()) ||
+        item.reviewTitle.toLowerCase().includes(search.toLowerCase())
       );
     }
 
     // Sort data
     if (sort) {
-      const [key, order] = (sort as string).split(',');
+      const [key, order] = sort.split(',');
       filteredData.sort((a, b) => {
-        if (order === 'asc') return a[key as keyof Evaluation] > b[key as keyof Evaluation] ? 1 : -1;
-        return a[key as keyof Evaluation] < b[key as keyof Evaluation] ? 1 : -1;
+        if (order === 'asc') return a[key] > b[key] ? 1 : -1;
+        return a[key] < b[key] ? 1 : -1;
       });
     }
 
     // Pagination
-    const startIndex = (Number(page) - 1) * Number(limit);
-    const paginatedData = filteredData.slice(startIndex, startIndex + Number(limit));
+    const startIndex = (page - 1) * limit;
+    const paginatedData = filteredData.slice(startIndex, startIndex + limit);
 
     return NextResponse.json({
       data: paginatedData,
       total: filteredData.length,
     });
   } catch (error) {
-    console.error('Error details:', error); // Log the error for debugging
-    return NextResponse.json({ error: error }, { status: 500 });
+    console.error('Error details:', error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
