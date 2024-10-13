8323cc3a35bfce711db07470fd88b880b5e2fc96
Ankur Srivastava <ankursrivastava@Ankurs-MacBook-Air.local>
2024-10-13 07:02:27 +0530

    message

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -199,7 +199,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1cdd071a96e60b42"; }
+/******/ 		__webpack_require__.h = function() { return "4dfcf0ef21394df8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2022-11-15',
+  apiVersion: '2024-09-30.acacia', 
 });
 
 export async function POST(req: NextRequest) {
